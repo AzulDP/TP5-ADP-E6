b1121d2adda070ac5faacfbe640fa80ab7b02365
Azul De Pol <azul.depol@gmail.com>
2023-04-15 19:56:21 +0100

    maquete los estilos de la pagina, los headings y los botones

--- js/TP5-ejercicio6.js
+++ js/TP5-ejercicio6.js
@@ -3,3 +3,10 @@
 6- Realizar una web con un temporizador donde el usuario pueda ingresar un tiempo desde donde comenzará a decrementar el contador. Debe contener los botones, iniciar, pausar y reset. 
 
 */
+
+
+
+reset.onclick = function() {
+    h1.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
+}
