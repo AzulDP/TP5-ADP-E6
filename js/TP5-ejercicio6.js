7526d342fa2560a90a76d31266d424b3e9ae8a3d
Azul De Pol <azul.depol@gmail.com>
2023-04-15 17:46:31 +0100

    agregue enunciado y readme

---
+++ js/TP5-ejercicio6.js
@@ -0,0 +1,5 @@
+/*Temporizador - Dificultad:  🟢🟡
+
+
+
+6- Realizar una web con un temporizador donde el usuario pueda ingresar un tiempo desde donde comenzará a decrementar el contador. Debe contener los botones, iniciar, pausar y reset.*/
